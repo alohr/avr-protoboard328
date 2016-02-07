3afa0de3776d48b22ac448ec5d6abc3dfd1bfd26
Andreas Lohr <andr3as.lohr@gmail.com>
2016-02-07 16:21:56 +0100

    removed serial code

--- examples/motortest/motortest.c
+++ examples/motortest/motortest.c
@@ -4,60 +4,6 @@
 
 #define TIMER2_PRESCALE_DIVIDER 8
 
-
-void setup_usart(void)
-{
-#ifndef BAUD
-#define BAUD 57600
-#endif
-#include <util/setbaud.h>
-    UBRR0H = UBRRH_VALUE; // from setbaud.h
-    UBRR0L = UBRRL_VALUE;
-#if USE_2X
-    UCSR0A |= _BV(U2X0);
-#else
-    UCSR0A &= ~_BV(U2X0);
-#endif
-    // enable tx and rx
-    UCSR0B = _BV(TXEN0) | _BV(RXEN0);
-    // 8 data bits
-    UCSR0C = _BV(UCSZ01) | _BV(UCSZ00);
-}
-
-void sendbyte(uint8_t data)
-{
-    loop_until_bit_is_set(UCSR0A, UDRE0);
-    UDR0 = data;
-}
-
-void sendstring(const char *s)
-{
-    if (s) {
-        while (*s != '\0')
-            sendbyte(*s++);
-    }
-}
-
-char tohex(uint8_t nibble) 
-{
-    return nibble < 10
-        ? '0' + nibble
-        : 'A' + nibble - 10;
-}
-
-void sendhexbyte(uint8_t byte)
-{
-    sendbyte(tohex((byte & 0xf0) >> 4));
-    sendbyte(tohex(byte & 0x0f));
-}
-
-void sendhexword(uint16_t word)
-{
-    sendhexbyte((word & 0xff00) >> 8);
-    sendhexbyte(word & 0xff);
-    sendbyte('\r');
-}
-
 void setup_timer2(void)
 {
     DDRB |= _BV(PB3); // OC2A pin
@@ -132,12 +78,13 @@ void setup(void)
 int main(void)
 {
     setup();
-xo    // setup_usart();
+
     PORTB |= _BV(PB2);
 
     for (;;) {
         timer2_set_oc2a(230);
         _delay_ms(2000);
+
         timer2_set_oc2a(255);
         _delay_ms(2000);
 
